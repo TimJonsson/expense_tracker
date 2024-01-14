aeaf57795080d2fececaff1b62dc562b1033a1d9
Tim Jonsson <tim.jonsson@easypark.net>
2024-01-14 13:26:26 +0100

    Removing main dart import in tests files for now

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:expense_tracker/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Expenses());
+    await tester.pumpWidget(const Expenses());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
