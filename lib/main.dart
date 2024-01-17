35cd7457867d4691ae820eaae9d8bd79dbe68786
Tim Jonsson <tim.jonsson@easypark.net>
2024-01-17 18:46:48 +0100

    Adding appbar and templating a modal overlay how to add an expense

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Expenses()
     ));
 }
-
-// void main() => runApp(const Quiz());
