9d50725053d03c8de0150c99932bdc454d471789
Tim Jonsson <tim.jonsson@easypark.net>
2024-01-19 09:46:00 +0100

    Adding fist UI for adding an expense

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -4,28 +4,75 @@ class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
   @override
-  State <NewExpense> createState() {
+  State<NewExpense> createState() {
     return _NewExpenseState();
   }
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
   @override
-  Widget build (BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
-              helperText: "Write digits",
-              label: Text("Hello"),
+            decoration: const InputDecoration(
+              label: Text("Enter title"),
             ),
           ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: const Text("Save"),
+              ),
+            ],
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text("Enter the amount"),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_amountController.text);
+                  },
+                  child: const Text("Save")),
+            ],
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Cancel"),
+          ),
+          const SizedBox(height: 20),
         ],
       ),
     );
   }
 }
-  
